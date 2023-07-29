aa4da1d3f5a698259fb5698489ceee4fecc13064
Bryan <bryanborck@gmail.com>
2023-07-29 16:50:41 -0300

    design header done

--- frontend/src/pages/Layout/Layout.tsx
+++ frontend/src/pages/Layout/Layout.tsx
@@ -4,10 +4,9 @@ import Header from '../../components/Header/Header';
 
 export default function Layout() {
     return (
-        <div className='w-[100vw] h-[100vh] bg-secondary-color flex flex-row overflow-hidden'>
+        <div className='w-[100vw] h-[100vh] bg-[#f6f6f6] flex flex-row overflow-hidden'>
             <Header />
             <main className='w-[100%] overflow-y-scroll'>
-                layout 
                 <Outlet />
             </main>
         </div>
