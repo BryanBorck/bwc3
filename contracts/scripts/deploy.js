96a069d80d8f70caf96851748b4c6ee0003c6f40
Luiz <luiz.junior@qitech.com.br>
2023-07-31 21:29:41 -0300

    creating NFT contract

--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -7,21 +7,13 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  const goodStremNFT = await hre.ethers.deployContract("GoodStreamCreator");
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
+  await goodStremNFT.waitForDeployment();
 
   console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+    `GoodStreamCreator deployed to ${goodStremNFT.target}`
   );
 }
 
