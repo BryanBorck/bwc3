0e566e3c97bb49c3c8e768c2cd7f8bd22cba2678
Daniel Yuki <danielyukihiga1@gmail.com>
2023-08-01 02:54:32 -0300

    course details backend

--- frontend/src/pages/Courses/CourseDetail.tsx
+++ frontend/src/pages/Courses/CourseDetail.tsx
@@ -4,6 +4,7 @@ import logo from "../../assets/goodstream_logoWHITE.png"
 import { connectMetamask } from '../../utils/connectMetamask';
 import { Framework } from '@superfluid-finance/sdk-core';
 import { ethers } from 'ethers';
+import axios from 'axios';
 
 export default function CourseDetails() {
     const { id } = useParams<{ id: string }>();
@@ -11,13 +12,45 @@ export default function CourseDetails() {
     const [enabled, setEnabled] = React.useState(false);
     const [money, setMoney] = React.useState(0);
 
-    const [course, setCourse] = React.useState({
-        // TESTE
-        id: id,
-        title: `Course ${id} aaa`,
-        description: `Course ${id} ddd description`,
-        cover: 'https://picsum.photos/seed/picsum/200/300'
-    });
+    // const [course, setCourse] = React.useState({
+    //     // TESTE
+    //     id: id,
+    //     title: `Course ${id} aaa`,
+    //     description: `Course ${id} ddd description`,
+    //     cover: 'https://picsum.photos/seed/picsum/200/300'
+    // });
+
+    const [course, setCourse] = React.useState<{
+        id: number,
+        author_name: string,
+        description: string,
+        category: string,
+        difficulty: string,
+        course_duration: number,
+        module_title: string,
+        module_link: string,
+        module_details: string,
+    }>();
+
+    React.useEffect(() => {
+        const url = `http://localhost:3001/courses/${id}`
+        axios.get(url).then(response => {
+            const tmp = {
+                id: response.data.id,
+                author_name: response.data.author_name,
+                description: response.data.description,
+                category: response.data.category,
+                difficulty: response.data.difficulty,
+                course_duration: response.data.course_duration,
+                module_title: response.data.module_title,
+                module_link: response.data.module_link,
+                module_details: response.data.module_details,
+            };
+
+            setCourse({...tmp});
+        })
+    }, [id])
+
 
     let rate = 1000000000000000;
     const ratePrint = 0.1
@@ -120,10 +153,10 @@ export default function CourseDetails() {
                     >&larr;
                         <span className='p-3 text-1.5rem'>Back</span>
                     </Link>
-                    <h1 className='my-[2.5vh] text-4xl text-secondary-color'>{course.title}</h1>
+                    <h1 className='my-[2.5vh] text-4xl text-secondary-color'>{course.module_title}</h1>
                     <div className='flex flex-row mb-[2.5vh] content-center'>
                         <img className='h-[2rem] w-[2rem] rounded-[100px] shadow-md' src={logo} alt="aaaa" />
-                        <h1 className='leading-[2rem] ml-[10px] align-middle text-primary-color'>Bry xxx xxx</h1>
+                        <h1 className='leading-[2rem] ml-[10px] align-middle text-primary-color'>{course.author_name}</h1>
                     </div>
                     <div className='flex flex-row w-full'>
                         <div className='w-[75%]'>
@@ -140,7 +173,7 @@ export default function CourseDetails() {
                                         pointerEvents: enabled ? 'all' : 'none',
                                         cursor: "pointer"
                                     }}
-                                    width="100%" height="500vh" src="https://www.youtube.com/embed/X6y42QbVSp4?start=1" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+                                    width="100%" height="500vh" src={course.module_link} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
                                 <div className='flex w-[100%] justify-between p-2 absolute bottom-1 items-center'>
                                     <p className='bg-white bg-opacity-25 py-1 px-2 rounded'>Amount paid: {Number(money).toFixed(4)}</p>
                                     <button onClick={(event) => deleteStreaming(event)} className='bg-red-500 hover:bg-red-600 text-white font-semibold py-1 px-2 rounded focus:outline-none focus:ring-2 focus:ring-red-500'>Delete Stream</button>
@@ -155,11 +188,11 @@ export default function CourseDetails() {
                                 <div className='flex flex-col items-start ml-[3%] w-[30%] p-[2vh] rounded-[15px] shadow-md bg-[#fff]'>
                                     <h2 className='text-fn text-black mb-[2vh]'>About this course</h2>
                                     <ul className='text-fs text-gray-400 w-full'>
-                                        <li className='py-[1vh]'>Category: Engineering</li>
+                                        <li className='py-[1vh]'>Category: {course.category}</li>
                                         <div className='h-[2px] bg-[#f6f6f6]' />
-                                        <li className='py-[1vh]'>Level: Intermediary</li>
+                                        <li className='py-[1vh]'>Level: {course.difficulty}</li>
                                         <div className='h-[2px] bg-[#f6f6f6]' />
-                                        <li className='py-[1vh]'>Hours: 12</li>
+                                        <li className='py-[1vh]'>Hours: {course.course_duration}</li>
                                     </ul>
                                 </div>
                             </section>
@@ -172,7 +205,7 @@ export default function CourseDetails() {
                                     <div className='h-[2px] mx-[1.5vh] bg-[#f6f6f6]' />
                                     <li className='text-fn pl-[2vh] py-[1.5vh] hover:bg-[#f6f6f6] cursor-pointer transition duration-600'>Modulo 2</li>
                                     <div className='h-[2px] mx-[1.5vh] bg-[#f6f6f6]' />
-                                    <li className='text-fn pl-[2vh] py-[1.5vh] hover:bg-[#f6f6f6] cursor-pointer transition duration-600'>Modulo 3</li>
+                                    <li className='text-fn pl-[2vh] py-[1.5vh] hover:bg-[#5c5050] cursor-pointer transition duration-600'>Modulo 3</li>
                                     <div className='h-[2px] mx-[1.5vh] bg-[#f6f6f6]' />
                                     <li className='text-fn pl-[2vh] py-[1.5vh] mb-[2vh] hover:bg-[#f6f6f6] cursor-pointer transition duration-600'>Modulo 4</li>
                                 </ul>
