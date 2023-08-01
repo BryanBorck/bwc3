d2286712b5496a55900a264bdd781d385039ed94
Luiz <luiz.junior@qitech.com.br>
2023-08-01 03:55:54 -0300

    final

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import ReactDom from 'react-dom'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './pages/Layout/Layout'
 import Home from './pages/Home/Home'
